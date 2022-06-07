171067773d149b3e57d3a3835796ffcb084f9bef
fuankan <3ryyno@gmail.com>
2022-06-08 04:02:59 +0600

    app(uitraining): pushed 1st business card ui

---
+++ chapter1/UItraining/app/src/main/java/kg/fkapps/ui_training/SecondCardActivity.kt
@@ -0,0 +1,11 @@
+package kg.fkapps.ui_training
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second_card)
+    }
+}
---
+++ chapter1/UItraining/app/src/main/java/kg/fkapps/ui_training/ThirdCardActivity.kt
@@ -0,0 +1,11 @@
+package kg.fkapps.ui_training
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ThirdCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third_card)
+    }
+}
