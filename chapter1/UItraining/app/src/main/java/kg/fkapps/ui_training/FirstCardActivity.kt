efc2624b6137661a36dc931c8158a222f703b69e
fuankan <3ryyno@gmail.com>
2022-06-10 10:03:31 +0600

    app(uitraining): pushed 2nd card's ui

--- chapter1/UItraining/app/src/main/java/kg/fkapps/ui_training/FirstCardActivity.kt
+++ chapter1/UItraining/app/src/main/java/kg/fkapps/ui_training/FirstCardActivity.kt
@@ -1,11 +1,32 @@
 package kg.fkapps.ui_training
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
 
 class FirstCardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_first_card)
+
+        val btnCall = findViewById<Button>(R.id.btn_call_first_card)
+//        val tvAddress = findViewById<TextView>(R.id.tv_address)
+
+        /*tvAddress.setOnClickListener {
+            val gmmIntentUri = Uri.parse("geo:42.878961, 74.627801")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            mapIntent.resolveActivity(packageManager)?.let {
+                startActivity(mapIntent)
+            }
+        }*/
+
+        btnCall.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "+996504772077"))
+            startActivity(intent)
+        }
     }
 }
--- chapter1/UItraining/app/src/main/java/kg/fkapps/ui_training/MainActivity.kt
+++ chapter1/UItraining/app/src/main/java/kg/fkapps/ui_training/MainActivity.kt
@@ -1,11 +1,32 @@
 package kg.fkapps.ui_training
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstBtn = findViewById<Button>(R.id.btn_first_card)
+        val secondBtn = findViewById<Button>(R.id.btn_second_card)
+        val thirdBtn = findViewById<Button>(R.id.btn_third_card)
+
+        firstBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, FirstCardActivity::class.java)
+            startActivity(intent)
+        }
+
+        secondBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, SecondCardActivity::class.java)
+            startActivity(intent)
+        }
+
+        thirdBtn.setOnClickListener {
+            val intent = Intent(this@MainActivity, ThirdCardActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
